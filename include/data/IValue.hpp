468405f036922be555f8c627d716e7b018a54761
drkwitht <derektan3510@gmail.com>
2024-05-03 17:01:57 -0700

    Wrote first JSON value impls.

---
+++ include/data/IValue.hpp
@@ -0,0 +1,26 @@
+#ifndef IVALUE_HPP
+#define IVALUE_HPP
+
+#include <any>
+
+namespace toyjson::data {
+
+    enum class JsonType {
+        j_null,
+        j_boolean,
+        j_number,
+        j_string,
+        j_array,
+        j_object
+    };
+
+    class IJsonValue {
+    public:
+        virtual ~IJsonValue() = default;
+
+        [[nodiscard]] virtual JsonType getType() const = 0;
+        [[nodiscard]] virtual std::any toBoxedValue() const = 0;
+    };
+}
+
+#endif
